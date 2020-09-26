c65e9005b68bc8a997b7e76035161600213f64c1
fcd <femnad@users.noreply.github.com>
2020-09-27 00:29:13 +0300

    update module

--- k8s.tf
+++ k8s.tf
@@ -1,14 +1,9 @@
 module "instance-module" {
   source               = "femnad/instance-module/gcp"
-  version              = "0.6.1"
+  version              = "0.7.1"
   github_user          = "femnad"
-  prefix               = "pre"
   project              = "foolproj"
   ssh_user             = "femnad"
-  preemptible          = true
-  service_account_file = ""
-  image                = "ubuntu-minimal-2004-focal-v20200917"
-  machine_type         = "e2-medium"
 }
 
 
