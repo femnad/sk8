87d43d067166773dbddfd79b8fdcd04f747819d8
fcd <femnad@users.noreply.github.com>
2020-09-26 22:15:15 +0300

    update machine size maybe?
    Does it help with PLEG error?

--- k8s.tf
+++ k8s.tf
@@ -1,21 +1,22 @@
 module "instance-module" {
-  source  = "femnad/instance-module/gcp"
-  version = "0.6.1"
-  github_user = "femnad"
-  prefix = "pre"
-  project = "foolproj"
-  ssh_user = "femnad"
-  preemptible = true
+  source               = "femnad/instance-module/gcp"
+  version              = "0.6.1"
+  github_user          = "femnad"
+  prefix               = "pre"
+  project              = "foolproj"
+  ssh_user             = "femnad"
+  preemptible          = true
   service_account_file = ""
-  image = "ubuntu-minimal-2004-focal-v20200917"
+  image                = "ubuntu-minimal-2004-focal-v20200917"
+  machine_type         = "e2-medium"
 }
 
 
 module "dns-module" {
-  source  = "femnad/dns-module/gcp"
-  version = "0.3.0"
-  dns_name = "k8s.fcd.dev."
+  source           = "femnad/dns-module/gcp"
+  version          = "0.3.0"
+  dns_name         = "k8s.fcd.dev."
   instance_ip_addr = module.instance-module.instance_ip_addr
-  managed_zone = "fcd-dev"
-  project = "foolproj"
+  managed_zone     = "fcd-dev"
+  project          = "foolproj"
 }
